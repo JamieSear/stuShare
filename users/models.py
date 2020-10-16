b4644bda11b9dff94ec406e7d37241782fb4a6d7
Jamie Sear <jamiesear@Jamie-Sears-MacBook.local>
2020-10-16 14:02:30 +0100

    managed to scale down images if they are too large

--- users/models.py
+++ users/models.py
@@ -2,12 +2,20 @@ from django.db import models
 from django.contrib.auth.models import User
 from PIL import Image
 
-# Create your models here.
 
 class Profile(models.Model):
-    user = models.OneToOneField(User, on_delete=models.CASCADE) #one to one relationship - if one is deleted, both will be
+    user = models.OneToOneField(User, on_delete=models.CASCADE) #if user is deleted then so is profile and vise versa
     image = models.ImageField(default='default.jpg', upload_to='profile_pics')
 
     def __str__(self):
-        return f'{self.user.username} Profile' #will print this
-        
+        return f'{self.user.username} Profile'
+
+    def save(self):
+        super().save()
+
+        img = Image.open(self.image.path)
+
+        if img.height > 300 or img.width > 300:
+            output_size = (300, 300)
+            img.thumbnail(output_size)
+            img.save(self.image.path)
