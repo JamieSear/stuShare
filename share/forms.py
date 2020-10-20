2472a82e4e2484e587ae956a15e8ff3e60368dc5
Jamie Sear <jamiesear@jamiesesmacbook.home>
2020-10-20 12:35:51 +0100

    removed name from comment form

--- share/forms.py
+++ share/forms.py
@@ -4,9 +4,9 @@ from .models import Comment
 class CommentForm(forms.ModelForm):
     class Meta:
         model = Comment
-        fields = ('name', 'body')
+        fields = ['body']
 
     widgets = {
-        'Username': forms.TextInput(attrs={'class': 'form-control'}),
+        # 'Username': forms.TextInput(attrs={'class': 'form-control'}),
         'Comment': forms.Textarea(attrs={'class': 'form-control'}),
     }
